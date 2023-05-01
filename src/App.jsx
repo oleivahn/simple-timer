c2c811ded07534b2597a4e160e2821ee48615115
Omar Leiva <oleivahn@gmail.com>
2023-05-01 06:16:37 -0400

    axios connected

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 import "./App.css";
 
 import ThemeBar from "./components/Themebar";
+import Home from "./components/Home";
 
 function App() {
   const [themeColor, setThemeColor] = useState(true);
@@ -21,7 +22,7 @@ function App() {
         <ThemeBar themeColor={themeColor} setThemeColor={setThemeColor} />
         {/* MAIN APP */}
         <div className="main-app-container">
-          <h1>Your content goes in here...</h1>
+          <Home />
         </div>
       </ThemeProvider>
     </div>
