bbab4eed552fae65cf186ddd35c63899af3f3b4a
Omar Leiva <oleivahn@gmail.com>
2023-05-01 03:21:39 -0400

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
 
-import { createTheme, ThemeProvider, CssBaseline, Paper } from "@mui/material";
-import Switch from "@mui/material/Switch";
-import FormControlLabel from "@mui/material/FormControlLabel";
-
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 import "./App.css";
 
-import WorkoutPage from "./components/WorkoutPage";
+import ThemeBar from "./components/Themebar";
 
 function App() {
   const [themeColor, setThemeColor] = useState(true);
@@ -21,24 +18,10 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <CssBaseline enableColorScheme />
-        <div id="theme-bar">
-          <Paper square elevation={3}>
-            <FormControlLabel
-              control={
-                <Switch
-                  id="theme-switch"
-                  checked={themeColor}
-                  color="success"
-                  onClick={() => setThemeColor(!themeColor)}
-                />
-              }
-              label="Theme - [App.jsx]"
-            />
-          </Paper>
-        </div>
+        <ThemeBar themeColor={themeColor} setThemeColor={setThemeColor} />
         {/* MAIN APP */}
         <div className="main-app-container">
-          <WorkoutPage />
+          <h1>Your content goes in here...</h1>
         </div>
       </ThemeProvider>
     </div>
