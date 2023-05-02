c5353a722dbd35a3ed95122a90072f24d0833fbd
Omar Leiva <oleivahn@gmail.com>
2023-05-01 23:29:10 -0400

    Axios and API hooked up

---
+++ README.md
@@ -0,0 +1,11 @@
+# How to run
+
+`npm run dev`
+
+Make sure the server is running... that's a different Repo
+[my-simple-timer-server]
+
+### Issues
+
+There's a CORs? issue with `Fetch` I think... That's why I used `Axios` instead.
+Try to fix this issue later
--- src/components/WorkoutPage.jsx
+++ src/components/WorkoutPage.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Timer from "./Timer";
 import Questions from "./Questions";
 import StopWatch from "./Stopwatch";
 
-import data from "../data/chestAndBack.json";
+// import data from "../data/chestAndBack.json";
 
 const WorkoutPage = () => {
+  const [data, setData] = useState({});
+
   const [pageNumber, setPageNumber] = useState(0);
   const [finished, setFinished] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api")
+      .then(function (response) {
+        // handle success
+        setData(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
 
   const handlePageNumber = () => {
     setPageNumber(pageNumber + 1);
@@ -36,19 +52,26 @@ const WorkoutPage = () => {
 
   return (
     <div>
-      <Questions data={data} pageNumber={pageNumber} />
-      <br />
-      {/* <StopWatch /> */}
-      <Timer
-        handlePageNumber={handlePageNumber}
-        pageNumber={pageNumber}
-        stepPageNumber={stepPageNumber}
-        resetPageNumber={resetPageNumber}
-        finished={finishedWorkoutHandler}
-        roundsToDo={data.length}
-      />
-      {finished && <p>CONGRATSSSS</p>}
-      {finished && <p>{`Summary: ${data ? JSON.stringify(data[0]) : null}`}</p>}
+      {data ? (
+        <>
+          <Questions data={data} pageNumber={pageNumber} />
+          {/* <StopWatch /> */}
+          <Timer
+            handlePageNumber={handlePageNumber}
+            pageNumber={pageNumber}
+            stepPageNumber={stepPageNumber}
+            resetPageNumber={resetPageNumber}
+            finished={finishedWorkoutHandler}
+            roundsToDo={data.length}
+          />
+          {finished && <p>CONGRATSSSS</p>}
+          {finished && (
+            <p>{`Summary: ${data ? JSON.stringify(data[0]) : null}`}</p>
+          )}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
