e1a47591d0f9916449465df53304bc5af901675b
Omar Leiva <oleivahn@gmail.com>
2023-05-04 04:51:55 -0400

    Finished setting up get and post endpoints connected to backend. Enough for scafold project

--- src/components/Endpoints.jsx
+++ src/components/Endpoints.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import testData from "../data/chestAndBack.json";
 
 function Endpoints() {
   const [serverResponse, setServerResponse] = useState();
 
   // Axios POST Example
+  const getFromEndpointHandler = async (e) => {
+    console.log("Getting data from endpoint...");
+
+    axios
+      .get("http://localhost:5000/startworkout")
+      .then(function (response) {
+        console.log("🚀 ~ file: WorkoutPage.jsx:83 ~ response:", response);
+        // handle success
+        setServerResponse(response.data[0]);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+
   const finishNowHandler = async (e) => {
-    const testData = {
-      title: "John Doe",
-      body: 30,
-    };
+    console.log(
+      "🚀 ~ file: Endpoints.jsx:15 ~ finishNowHandler ~ testData:",
+      testData
+    );
     console.log("Finish now...", e.target.value);
 
     axios
@@ -26,8 +43,6 @@ function Endpoints() {
         // handle error
         console.log(error);
       });
-
-    setFinished(true);
   };
 
   return (
@@ -37,14 +52,20 @@ function Endpoints() {
       </p>
       <div>Endpoints page</div>
       <div style={{ marginTop: "25px" }}>
+        <button onClick={getFromEndpointHandler} value="get-now">
+          Get...
+        </button>
+      </div>
+      <div style={{ marginTop: "25px" }}>
         <button onClick={finishNowHandler} value="finish-now">
           Post...
         </button>
-        <p
-          style={{ marginTop: "25px", color: "#195d85" }}
-        >{`Server Response coming from Mongoose too: ${
+        <p style={{ marginTop: "25px", color: "orange" }}>
+          Server Response coming from Mongoose too:{" "}
+        </p>
+        <p style={{ marginTop: "10px", color: "green" }}>{`${
           serverResponse
-            ? `${serverResponse.title} and ${serverResponse.body}`
+            ? `${serverResponse.user} and ${serverResponse.workout}`
             : "Click the button to see the response"
         }`}</p>
       </div>
