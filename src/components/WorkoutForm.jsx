6040d50bd7ab0ad822e0d13c83402116285085db
Omar Leiva <oleivahn@gmail.com>
2023-09-23 08:54:39 -0400

    even buttons

--- src/components/WorkoutForm.jsx
+++ src/components/WorkoutForm.jsx
@@ -148,7 +148,7 @@ const WorkoutForm = ({ data, pageNumber }) => {
                       required
                       className="block w-full rounded-md border-0 px-3 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     /> */}
-                    <select className="select select-bordered w-full max-w-xs">
+                    <select className="select select-bordered w-full max-w-xs ring-1 ring-inset ring-gray-300">
                       <option selected>{data[round].typeOfWeight}</option>
                       <option>Dumbells</option>
                       <option>Yellow Bands</option>
